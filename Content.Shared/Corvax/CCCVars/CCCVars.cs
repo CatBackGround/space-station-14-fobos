d91406b0243dfefd55406157ba5739a4acd24b1d
CatBackGround <115424457+CatBackGround@users.noreply.github.com>
2025-06-15 22:03:56 +0500

    Update CCCVars.cs

--- Content.Shared/Corvax/CCCVars/CCCVars.cs
+++ Content.Shared/Corvax/CCCVars/CCCVars.cs
@@ -74,15 +74,18 @@ public sealed class CCCVars
         CVarDef.Create("tts.rate_limit_count", 3, CVar.SERVERONLY);
 
     /*
-     * Peaceful Round End
+     * Peaceful Round End. DEADSPACE: Deleted.
      */
 
     /// <summary>
     /// Making everyone a pacifist at the end of a round.
     /// </summary>
+    
+    /*
     public static readonly CVarDef<bool> PeacefulRoundEnd =
         CVarDef.Create("game.peaceful_end", true, CVar.SERVERONLY);
-
+     */
+    
     /*
      * Discord Auth
      */
